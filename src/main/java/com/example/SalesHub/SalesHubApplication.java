87fcf4ac5fc44946d9dc9dfe6d1a0221ced5c7b7
Dev-lucass <lucasbitencourt959@gmail.com>
2026-01-23 15:09:36 -0300

    criando repositorio

---
+++ src/main/java/com/example/SalesHub/SalesHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SalesHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesHubApplication.class, args);
+	}
+
+}
